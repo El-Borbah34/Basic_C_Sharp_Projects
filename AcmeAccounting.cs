3ad909058b9100f551983a82fbeb215bfc922dd1
Pierson Han <piersonhan@gmail.com>
2020-03-07 11:40:27 -0800

    first CS program

---
+++ AcmeAccounting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+        Console.ReadLine();
+        }
+    }
+
