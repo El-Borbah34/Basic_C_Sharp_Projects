9751b05f8c481dec149338fe377cf3e05ab0b2c4
Pierson Han <piersonhan@gmail.com>
2020-03-17 13:04:59 -0700

    Add Folder OptionalParameter

---
+++ OptionalParameterAssignment/OptionalParameterAssignment/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterAssignment
+{
+    public class Calculator
+    {
+        public int adding(int num, int num2 = 0)
+        {
+            int newNum = num + 5 + num2;
+            return newNum;
+        }
+    }
+}
---
+++ OptionalParameterAssignment/OptionalParameterAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Please give two numbers one at a time when prompted, please know that a second number is not need to run this program.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now what is that second number? (Just hit enter if you don't want to provide a second number.)");
+            string secondNum = Console.ReadLine();
+            int finishNum = string.IsNullOrEmpty(secondNum) ?
+                calculator.adding(userNum) :
+                calculator.adding(userNum, Convert.ToInt32(secondNum));
+            Console.WriteLine("Your number(s) added eqauls: " + finishNum);
+
+            Console.ReadLine();
+        }
+    }
+}
