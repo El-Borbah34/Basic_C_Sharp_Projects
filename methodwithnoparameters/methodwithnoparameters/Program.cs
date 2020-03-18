3c002a0b7e96f275c0f75d1ac2e56212b861c723
Pierson Han <piersonhan@gmail.com>
2020-03-17 23:17:10 -0700

    Folder Methodwithoutparameters

---
+++ methodwithnoparameters/methodwithnoparameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodwithnoparameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Give a number and we will return it divided by 2.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            int y;
+
+            calculator.getValue(userNum, out y);
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+            int random;
+            int random2;
+            calculator.getValue(userNum, out random, out random2);
+            Console.WriteLine(random);
+            Console.WriteLine(random2);
+            Console.ReadLine();
+
+            Calculator Abacus = new Calculator();
+
+        }
+
+        
+    }
+}
---
+++ methodwithnoparameters/methodwithnoparameters/calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodwithnoparameters
+{
+    public class Calculator
+    {
+        public void getValue(int inputNumber, out int outputNumber)
+        {
+            outputNumber = inputNumber / 2;
+        }
+
+        public void getValue(int userInput, out int outputNumber, out int outputNumber2)
+        {
+            outputNumber = userInput + 100;
+            outputNumber2 = outputNumber + userInput;
+        }
+    }
+    public static class Abacus
+    {
+        public static int adder(int num, int num2)
+        {
+            int combined = num + num2;
+            return combined;
+        }
+    }
+}
