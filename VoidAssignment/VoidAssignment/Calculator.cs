1234135f9b5e04ff78d051ec59293d693fef9774
Pierson Han <piersonhan@gmail.com>
2020-03-17 19:48:51 -0700

    Add VoidAssignment Folder

---
+++ VoidAssignment/VoidAssignment/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidAssignment
+{
+    public class Calculator
+    {
+        public void divide(int num1,int num2)
+        {
+            int newNum1 = num1 / 2;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ VoidAssignment/VoidAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Write out a whole number.");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now write out a second number.");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+            calculator.divide(num1: userNum1, num2: userNum2);
+            Console.ReadLine();
+        }
+    }
+}
