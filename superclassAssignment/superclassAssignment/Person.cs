d6a74207136b98f7f63a161b04048875f288bf4d
Pierson Han <piersonhan@gmail.com>
2020-03-18 10:35:18 -0700

    Add Folder SuperClassAssignment

---
+++ superclassAssignment/superclassAssignment/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superclassAssignment
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: "+ firstName + " " + lastName);
+        }
+    }
+}
---
+++ superclassAssignment/superclassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superclassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
