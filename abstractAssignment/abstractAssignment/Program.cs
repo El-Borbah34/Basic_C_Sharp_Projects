8321ff6193f6e301f51bd851cf92e53cf51e7816
Pierson Han <piersonhan@gmail.com>
2020-03-18 17:21:40 -0700

    Add folder abstractAssignment

---
+++ abstractAssignment/abstractAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.WriteLine("What is a good first name?");
+            employee.firstName = Console.ReadLine();
+            Console.WriteLine("Now choose a last name.");
+            employee.lastName = Console.ReadLine();
+
+            employee.SayName();
+
+            Console.WriteLine("I didn't use \"Sample\" \"Student\" because I wanted to actully use it." );
+            Console.ReadLine();
+        }
+    }
+}
