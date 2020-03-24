e0fc8e101db26a59d1ec314627cccfc29a8057bb
Pierson Han <piersonhan@gmail.com>
2020-03-24 10:00:27 -0700

    StructAssignment

---
+++ structAssignment/structAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            numbers.Amount = 34.34M;
+            decimal actualAmount = numbers.Amount; //did this just cause.
+
+            Console.WriteLine(actualAmount);
+            Console.ReadLine();
+        }
+    }
+}
